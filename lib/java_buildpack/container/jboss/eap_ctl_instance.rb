f6f7b1d4057dca1ae628c5a78440eb1abb058f80
jliptak <jliptak@centurylink.com>
2015-03-27 16:26:50 -0600

    remove debugging for staging remove file logging

--- lib/java_buildpack/container/jboss/eap_ctl_instance.rb
+++ lib/java_buildpack/container/jboss/eap_ctl_instance.rb
@@ -122,15 +122,11 @@ module JavaBuildpack
 
 
       def expand(file)
-        puts "Expanding... unzip  #{file.path} -d #{@droplet.sandbox}  2>&1"
-#        with_timing "Expanding EAP to #{@droplet.sandbox.relative_path_from(@droplet.root)}" do
-          puts "makedir ..."
+        with_timing "Expanding EAP to #{@droplet.sandbox.relative_path_from(@droplet.root)}" do
           FileUtils.mkdir_p @droplet.sandbox
-          puts "unzip... "
           shell "unzip -q '#{file.path}' -d #{@droplet.sandbox} 2>&1"
-          puts "copy_resources"
           @droplet.copy_resources
-#        end
+        end
       end
 
 
