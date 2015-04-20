127e92b538f76c29a8b0f1022e48d20a6c3701e0
jliptak <jliptak@X7012473.ctl.intranet>
2015-04-20 13:53:34 -0600

    put in background

--- lib/java_buildpack/container/jboss/eap_ctl_instance.rb
+++ lib/java_buildpack/container/jboss/eap_ctl_instance.rb
@@ -154,9 +154,9 @@ module JavaBuildpack
       end
 
       def cli
-         shell  "$PWD/#{(@droplet.sandbox + 'bin/standalone.sh')} -b=0.0.0.0 --admin-only"
+         shell  "#{(@droplet.sandbox + 'bin/standalone.sh')} -b=0.0.0.0 --admin-only &"
          shell  "sleep 60"
-         shell  "find $PWD -name '*.cli' -exec echo jboss-cli.sh -c --file={} \; | sort | bash"
+         shell  "find / -name '*.cli' -exec echo jboss-cli.sh -c --file={} \; | sort | bash"
       end
     end
 
